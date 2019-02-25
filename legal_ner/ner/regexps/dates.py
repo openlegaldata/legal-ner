e128ce7e843f5c24d5c8fa32ffd9dae03911adc3
langhabel <langhabel@users.noreply.github.com>
2019-02-25 20:02:05 +0100

    Fix problems in dates regexp

--- legal_ner/ner/regexps/dates.py
+++ legal_ner/ner/regexps/dates.py
@@ -8,8 +8,8 @@ from legal_ner.ner.regexps.base import Regexp
 class GermanDates(Regexp):
 
     def regexp_obj(self):
-        return re.compile(r'\b([0123]?\d\. ?)?'
-                          r'(([01]?\d\.)|((Jan|Feb|Mrz|Mä|Apr|Mai|Jun|Jul|Aug|Sep|Okt|Nov|Dez)[a-z]{0,6}\.? ))'
+        return re.compile(r'\b((0?[1-9]|[123]\d)\. ?)?'
+                          r'(((0?[1-9]|1[012])\.)|((Jan|Feb|Mrz|Mä|Apr|Mai|Jun|Jul|Aug|Sep|Okt|Nov|Dez)[a-z]{0,6}\.? ))'
                           r'([12]\d)?\d{2}\b')
 
     def normalize(self, groups: dict, full_match: str):
